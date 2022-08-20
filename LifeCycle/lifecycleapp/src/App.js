6b8a0512c0381c527d98a8e4901c08a160ca250e
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2022-08-20 18:15:39 +0530

    Liyecycle

--- LifeCycle/lifecycleapp/src/App.js
+++ LifeCycle/lifecycleapp/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ConstructorMethod from './components/Mounting/constructorMethod'
+import GetDerivedStateFromPropsMethod from './components/Mounting/getDerivedStateFromPropsMethod'
+import RenderMethod from './components/Mounting/renderMethod'
+import ComponentDidMountMethod from './components/Mounting/componentDidMountMethod'
+import ShouldComponentUpdateMethod from './components/Updating/shouldComponentUpdateMethod'
+import GetSnapshotBeforeUpdateMethod from './components/Updating/getSnapshotBeforeUpdateMethod'
+import ComponentDidUpdateMethod from './components/Updating/componentDidUpdateMethod'
+import ComponentWillUnmountMethod from './components/Unmounting/componentWillUnmount'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <h1>Mounting</h1>
+      <h2>constructor Method</h2>
+       <ConstructorMethod />
+       <h2>getDerivedStateFromProps Method</h2>
+       <GetDerivedStateFromPropsMethod />
+       <h2>render Method</h2>
+       <RenderMethod />
+       <h2>componentDidMount Method</h2>
+       <ComponentDidMountMethod/>
+       <h1>Updating</h1>
+       <h2>shouldComponentUpdate Method</h2>
+       <ShouldComponentUpdateMethod />
+       <h2>getSnapshotBeforeUpdate Method</h2>
+       <GetSnapshotBeforeUpdateMethod />
+       <h2>componentDidUpdate Method</h2>
+       <ComponentDidUpdateMethod />
+       <h1>Unmounting</h1>
+       <h2>componentWillUnmount Method </h2>
+       <ComponentWillUnmountMethod />
+      </>
+    )
+  }
 }
 
-export default App;
