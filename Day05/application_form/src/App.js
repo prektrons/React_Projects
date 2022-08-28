021e3bfe4425d6ce3988f94ca7788c9bcc9e3488
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2022-08-28 22:13:37 +0530

    Step 1 — Creating a Basic Form with JSX

--- Day05/application_form/src/App.js
+++ Day05/application_form/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+   setSubmitting(true);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 3000)
+ }
+
+  return(
+    <div className="wrapper">
+      <h1> Apples</h1>
+      {submitting &&
+       <div>Submtting Form...</div>
+     }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
