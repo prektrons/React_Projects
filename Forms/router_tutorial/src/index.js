d3bf1af58d0c1d4be6e0d8f6e40ef86819f41956
Preksha Sharma <46593513+prektrons@users.noreply.github.com>
2022-08-31 16:49:46 +0530

    Final

--- Forms/router_tutorial/src/index.js
+++ Forms/router_tutorial/src/index.js
@@ -1,23 +1,39 @@
-import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import App from "./App";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-root.render(
+const rootElement = document.getElementById('app');
+ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
+  rootElement
 );
